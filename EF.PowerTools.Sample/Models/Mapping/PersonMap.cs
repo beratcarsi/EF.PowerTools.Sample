73a3c0e5ad70488225191308e7b429c07da96dd8
Berat Çarşı <beratcarsi@gmail.com>
2016-08-29 17:09:19 +0300

    init

---
+++ EF.PowerTools.Sample/Models/Mapping/PersonMap.cs
@@ -0,0 +1,36 @@
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.ModelConfiguration;
+
+namespace EF.PowerTools.Sample.Models.Mapping
+{
+    public class PersonMap : EntityTypeConfiguration<Person>
+    {
+        public PersonMap()
+        {
+            // Primary Key
+            this.HasKey(t => t.PersonID);
+
+            // Properties
+            this.Property(t => t.LastName)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            this.Property(t => t.FirstName)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            this.Property(t => t.Discriminator)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            // Table & Column Mappings
+            this.ToTable("Person");
+            this.Property(t => t.PersonID).HasColumnName("PersonID");
+            this.Property(t => t.LastName).HasColumnName("LastName");
+            this.Property(t => t.FirstName).HasColumnName("FirstName");
+            this.Property(t => t.HireDate).HasColumnName("HireDate");
+            this.Property(t => t.EnrollmentDate).HasColumnName("EnrollmentDate");
+            this.Property(t => t.Discriminator).HasColumnName("Discriminator");
+        }
+    }
+}
